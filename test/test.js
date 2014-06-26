f5234c33df89e74bdf1c5e7c45eb6dd06ed579e4
stebru <stefan@colours.no>
2014-06-26 12:01:17 +0200

    Start testing

--- test/test.js
+++ test/test.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 
-    QUnit.test('Stackbox test', function(assert) {
-        assert.ok(typeof null === 'object', 'But, of course!');
+    QUnit.module('Core', {
+        setup: function() {
+            $('#open-stackbox').modalBox();
+        },
+        teardown: function() {}
     });
+    QUnit.test('Stackbox core', function(assert) {
 
+        var $mainWrapper = $('.modals');
+        assert.strictEqual(1, $mainWrapper.length, 'Only one base wrapper');
+
+        var $wrapper = $mainWrapper.find('.modal-wrapper');
+        assert.strictEqual(1, $wrapper.length, 'Only one wrapper');
+
+        var $modal = $wrapper.find('.modal');
+        assert.strictEqual(1, $modal.length, 'Only one modal');
+    });
 });
