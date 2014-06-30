1be89db99629f30b8bf94c7cd02f0573d329d01f
Stefan Bruvik <stefan@colours.no>
2014-06-30 20:05:35 +0200

    Remove demo folder (docs are better)

--- demo/global.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $.fn.stackbox.globalSettings = {
-        spinnerClass: 'fa fa-cog fa-spin fa-5x',
-        closeButtonIcon: '<i class="fa fa-times"></i>'
-    };
-});
