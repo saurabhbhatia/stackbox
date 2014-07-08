4b4bb7bb5c44891f20ca7661daadb32c4f1ea2a8
stebru <stefan@colours.no>
2014-07-08 23:44:00 +0200

    Reset

--- grunt/qunit.js
+++ grunt/qunit.js
@@ -1,11 +1,5 @@
 // https://www.npmjs.org/package/grunt-contrib-qunit
 
 module.exports = {
-    options: {
-        timeout: 5000,
-        urls: [
-            'http://127.0.0.1:8000/test/index.html',
-        ]
-    },
-    all: ['http://127.0.0.1:8000/test/index.html']
+    all: ['test/index.html']
 };
