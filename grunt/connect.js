3d999a9d51d7730bba011c2df5eac59a9fa856e6
stebru <stefan@colours.no>
2014-07-08 22:50:09 +0200

    Connect

---
+++ grunt/connect.js
@@ -0,0 +1,10 @@
+// https://www.npmjs.org/package/grunt-contrib-connect
+
+module.exports = {
+    server: {
+        options: {
+            port: 3000,
+            base: '.'
+        }
+    }
+};
--- grunt/qunit.js
+++ grunt/qunit.js
@@ -1,5 +1,10 @@
 // https://www.npmjs.org/package/grunt-contrib-qunit
 
 module.exports = {
+    options: {
+        urls: [
+            'http://localhost:3000/test/index.html',
+        ]
+    },
     all: ['test/index.html']
 };
